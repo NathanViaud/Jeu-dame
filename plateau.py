829eea928618594c7dd17b9cab2051fa6f3177ea
NathanViaud <79544144+NathanViaud@users.noreply.github.com>
2023-01-30 20:31:41 +0100

    new Logic not tested yet

---
+++ plateau.py
@@ -0,0 +1,76 @@
+class Plateau:
+    def __init__(self):
+        self.cacheEliminations = []
+        self.plateau = [None] * 51
+        for i in range(1, 21):
+            self.plateau[i] = (0, False)
+        for i in range(31, 51):
+            self.plateau[i] = (1, False)
+    
+    def deplacementBase(self, posPion):
+        res = []
+        isPairline = posPion%10 <= 5
+        isLeftOutside = posPion%5 == 1 and not isPairline
+        isRightOutside = posPion%5 == 0 and isPairline
+        if(self.plateau[posPion][0] == 1 or self.plateau[posPion][1]):
+            if(not isRightOutside):
+                possiblePos = posPion + 6 if isPairline else posPion + 5
+                res.append(possiblePos)
+            if(not isLeftOutside):
+                possiblePos = posPion + 5 if isPairline else posPion + 4
+                res.append(possiblePos)
+        if(self.plateau[posPion][0] == 0 or self.plateau[posPion][1]):
+            if(not isLeftOutside):
+                possiblePos = posPion - 5 if isPairline else posPion - 6
+                res.append(possiblePos)
+            if(not isRightOutside):
+                possiblePos = posPion - 4 if isPairline else posPion - 5
+                res.append(possiblePos)
+        return res
+    
+    def eliminationPossible(self, pos1, pos2):
+        res = None
+        isPairline = pos1%10 <= 5
+        isUpward = pos1 > pos2
+        isRight = False
+        if(isUpward) :
+            isRight = pos1 == pos2 + 4 if isPairline else pos1 == pos2 + 5
+            if(isRight):
+                finalPos = pos1 - 9
+                if self.plateau[finalPos] == None : return (finalPos, pos2)
+            else:
+                finalPos = pos1 - 11
+                if self.plateau[finalPos] == None : return (finalPos, pos2)
+        else:
+            isRight = pos1 == pos2 - 5 if isPairline else pos1 == pos2 - 4
+            if(isRight):
+                finalPos = pos1 + 11
+                if self.plateau[finalPos] == None : return (finalPos, pos2)
+            else:
+                finalPos = pos1 + 9
+                if self.plateau[finalPos] == None : return (finalPos, pos2)
+        return res
+    
+    def deplacementPossible(self, posPion):
+        res = []
+        self.cacheEliminations = []
+        baseDeplacemnts = self.deplacementBase(posPion)
+        for deplacement in baseDeplacemnts:
+            if self.plateau[deplacement] == None:
+                res.append(deplacement)
+            else:
+                elimination = self.eliminationPossible(posPion, deplacement)
+                if elimination != None:
+                    self.cacheEliminations.append(elimination)
+                    res.append(elimination[0])
+        return res
+    
+    def deplacer(self, posPion, newPos):
+        if newPos in self.deplacementPossible(posPion):
+            self.plateau[newPos] = self.plateau[posPion]
+            self.plateau[posPion] = None
+
+            #? Elimination
+            for elimination in self.cacheEliminations:
+                if elimination[0] == newPos:
+                    self.plateau[elimination[1]] = None
